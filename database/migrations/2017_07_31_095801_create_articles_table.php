c6dac7fe74e5868d853742e75085530971abe51f
Rajpraiyalan Jesuthasan <raj.cemplon@jeylabs.com>
2017-07-31 15:37:52 +0530

    migration, model files & relations - Author & Articles

---
+++ database/migrations/2017_07_31_095801_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('author_id');
+            $table->string('title');
+            $table->string('url')->unique();
+            $table->text('content');
+            $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
